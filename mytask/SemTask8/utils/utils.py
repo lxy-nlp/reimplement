c05480606493157fcf8036258763bbbfbdb94f86
lxy <18261195602@163.com>
2021-03-18 22:55:09 +0800

    修复bug

--- mytask/SemTask8/utils/utils.py
+++ mytask/SemTask8/utils/utils.py
@@ -13,6 +13,9 @@ from nltk.corpus import wordnet
 
 
 # 探索类别的分布
+from mytask.SemTask8.utils import constant
+
+
 def data_exploration(file):
     contents = pd.read_table(file)
     labels = contents.iloc[:,1].to_list()
@@ -26,12 +29,21 @@ def data_exploration(file):
         result[i] = labels_index.count(i)
     return result
 
+def sequence_length(file):
+    with open(file, 'r') as f:
+        lines = f.readlines()
+    all_length = []
+    for i, line in enumerate(lines):
+        line_split = line.split()
+        all_length.append(len(line_split))
+    return all_length
+
 # 使用Wordnet增强数据
 def data_argument(items):
     pass
 
 
-def pos_encoding(items, vocab_size,max_len=30):
+def pos_encoding(items, vocab_size):
     '''
 
     :param items: data_item list
@@ -47,8 +59,64 @@ def pos_encoding(items, vocab_size,max_len=30):
     pos_embedding /= len(items)
     return pos_embedding
 
+def evaluate_batch_based(predicted_batch, gold_batch, threshold = 1.0, idx2label=None, empty_label = None):
+    if len(predicted_batch) != len(gold_batch):
+        raise TypeError("predicted_idx and gold_idx should be of the same length.")
+
+    correct = 0
+    for i in range(len(gold_batch)):
+        rec_batch = micro_avg_precision(predicted_batch[i], gold_batch[i], empty_label)
+        if rec_batch >= threshold:
+            correct += 1
+
+    acc_batch = correct / float(len(gold_batch))
+
+    return acc_batch
 
 
+def evaluate_instance_based(predicted_idx, gold_idx, idx2label=None, empty_label=None):
+    if len(predicted_idx) != len(gold_idx):
+        raise TypeError("predicted_idx and gold_idx should be of the same length.")
+    if idx2label:
+        label_y = [idx2label[element] for element in gold_idx]
+        pred_labels = [idx2label[element] for element in predicted_idx]
+    else:
+        label_y = gold_idx
+        pred_labels = predicted_idx
+
+    prec = micro_avg_precision(pred_labels, label_y, empty_label)
+    rec = micro_avg_precision(label_y, pred_labels, empty_label)
+
+    f1 = 0
+    if (rec + prec) > 0:
+        f1 = 2.0 * prec * rec / (prec + rec)
+
+    return prec, rec, f1
+
+
+def micro_avg_precision(guessed, correct, empty=None):
+    """
+    Tests:
+    >>> micro_avg_precision(['A', 'A', 'B', 'C'],['A', 'C', 'C', 'C'])
+    0.5
+    >>> round(micro_avg_precision([0,0,0,1,1,1],[1,0,1,0,1,0]), 6)
+    0.333333
+    """
+    correctCount = 0
+    count = 0
+
+    idx = 0
+    while idx < len(guessed):
+        if guessed[idx] != empty:
+            count += 1
+            if guessed[idx] == correct[idx]:
+                correctCount += 1
+        idx += 1
+    precision = 0
+    if count > 0:
+        precision = float(correctCount) / count
+
+    return precision
 
 if __name__ == '__main__':
     '''
@@ -56,7 +124,11 @@ if __name__ == '__main__':
     没有关系的类别有1003个 其余的类别分布较为不均匀
     因此想到使用 EDA中的同义词替换 扩充数据
     '''
+    all_length = sequence_length('../datas/SemEval2010-Task8/train/train.txt')
+    print(max(all_length),min(all_length))
    # print(data_exploration('../datas/SemEval2010-Task8/train/train_result.txt'))
-    print(wordnet.synsets('start'))  # 同义词替换的问题 词性
+   # print(wordnet.synsets('start'))  # 同义词替换的问题 词性
+
+
 
 
--- mytask/SemTask8/utils/vocab.py
+++ mytask/SemTask8/utils/vocab.py
@@ -11,6 +11,8 @@ import numpy as np
 import pandas as pd
 import pickle
 import re
+import torch.utils.data as Data
+import torch
 
 from mytask.SemTask8.utils import constant
 
@@ -22,9 +24,8 @@ class Vocab:
         if load:
             assert os.path.exists(filename),"Vocab file does not exist at " + filename
 
-            self.id2word,self.word2id,self.emdeddings = self.load_glove_vocab(filename, wv_dim)
+            self.id2word,self.word2id, self.emdeddings = self.load_glove_vocab(filename, wv_dim)
             self.size = len(self.id2word)
-
         else:
             print("Creating vocab from scratch...")
             assert word_counter is not None, "word_counter is not provided for vocab creation."
@@ -58,8 +59,8 @@ class Vocab:
         word2id = {w : i for i, w in zip(id2word.keys(),id2word.values())}
         vocab_size = len(vocab)
         emb = np.random.uniform(-1, 1, (vocab_size + 2, wv_dim))
-        emb[constant.PAD_ID] = 0  # <pad> should be all 0
-        emb[constant.UNK_ID] = 1
+        emb[constant.PAD_ID] = [0]*wv_dim  # <pad> should be all 0
+        emb[constant.UNK_ID] = [1]*wv_dim
         with open(file, encoding="utf8") as f:
             for line in f:
                 elems = line.split()
@@ -91,13 +92,15 @@ class Vocab:
 
 
 class data_item:
-    def __init__(self, sub, obj, sentence,label,label_id):
+    def __init__(self, sub, obj, sentence, label, label_id):
         self.sub = sub
         self.obj = obj
         self.sentence = sentence
         self.sent_ids = []
         self.label = label
         self.label_id = label_id
+        self.pos_encoding = None
+        self.mask = None
 
     def map(self, token_list, word2id):
         """
@@ -106,6 +109,30 @@ class data_item:
         self.sent_ids = [word2id[w] if w in word2id else constant.UNK_ID for w in token_list]
         return self.sent_ids
 
+    # 位置编码
+    def pos_encode(self):
+        if len(self.sentence) < 1:
+            raise NotImplementedError
+        pos_embedding = np.ones((len(self.sentence), 4))
+        for i, item in zip(range(0, len(self.sentence)), self.sentence):
+            pos_embedding[i][0] = i
+            pos_embedding[i][1] = i - self.sub[0]
+            pos_embedding[i][2] = i - self.obj[0]
+            pos_embedding[i][3] = self.sub[0] - self.obj[0]
+        pos_embedding /= len(self.sentence)
+        self.pos_encoding = pos_embedding
+        return pos_embedding
+
+    # 从sequence_list中挖去  主体 和 客体
+    def mask_entity(self):
+        mask_matrix = [1] * len(self.sentence)
+        sub_mask = [0] * len(self.sentence)
+        obj_mask = [0] * len(self.sentence)
+        mask_matrix[self.sub[0]:self.sub[1]+1] = [0] * (self.sub[1] - self.sub[0] + 1)
+        mask_matrix[self.obj[0]:self.obj[1]+1] = [0] * (self.obj[1] - self.obj[0] + 1)
+        sub_mask[self.sub[0]:self.sub[1]+1] = [1] * (self.sub[1] - self.sub[0] + 1)
+        obj_mask[self.obj[0]:self.obj[1]+1] = [1] * (self.obj[1] - self.obj[0] + 1)
+        return mask_matrix, sub_mask, obj_mask
 
 def read_labels(path):
     '''
@@ -113,7 +140,7 @@ def read_labels(path):
     :param path:
     :return: 原始标签和标签id
     '''
-    contents = pd.read_table(path)
+    contents = pd.read_table(path,header=None)
     labels = contents.iloc[:, 1].to_list()
     undep = set(labels)
     indexlist = range(0, len(undep))
@@ -122,7 +149,7 @@ def read_labels(path):
     labels_index = [label2id[label] for label in labels]
     return labels, labels_index
 
-def read_datas(path, word2id,labels, labels_index):
+def read_datas(path, word2id, labels, labels_index, max_len=70):
     '''
     用于处理数据
     :param path: 数据集路径
@@ -131,33 +158,70 @@ def read_datas(path, word2id,labels, labels_index):
     '''
     with open(path, 'r') as f:
         lines = f.readlines()
-    all_datas = []
+
+    all_items = []
+    all_pos = []
     for i, line in enumerate(lines):
+        single_item = []
         line_split = line.split()
         sub = []  # 主体的 首尾位置
         obj = []  # 客体的 首尾位置
         for i, item in zip(range(0, len(line_split)), line_split):
             if '<e1>' in item:
                 sub.append(i)
+                # all_sub_start.append(i)
             if '</e1>' in item:
                 sub.append(i)
+                # all_sub_end.append(i)
             if '<e2>' in item:
                 obj.append(i)
+                # all_obj_start.append(i)
             if '</e2>' in item:
                 obj.append(i)
+                # all_obj_end.append(i)
         line = re.sub(r'<e.>', '', line)
         line = re.sub(r'<.e.>', '', line)
         sequence = line.split()
-        one_item = data_item(sub, obj, sequence, labels[i],labels_index[i])
-        one_item.map(sequence, word2id)
-        all_datas.append(one_item)
-    return all_datas
+        sequence = sequence[:max_len] if len(sequence) >= max_len else sequence + ['PAD'] * (max_len - len(sequence))
+        # sequence = sequence[:max_len] if len(sequence) > max_len else sequence
+        one_item = data_item(sub, obj, sequence, labels[i], labels_index[i])
+        sentence_ids = one_item.map(sequence, word2id)
+        mask_matrix, sub_mask, obj_mask = one_item.mask_entity()
+        pos_embedding = one_item.pos_encode()
+        single_item.append(sentence_ids)
+        single_item.append(sub_mask)
+        single_item.append(obj_mask)
+        single_item.append(mask_matrix)
+        all_pos.append(pos_embedding)
+        all_items.append(single_item)
+    return all_items, labels_index, all_pos # 返回值是 训练数据 和 标签
 
+def load_data(batch_size,items,labels,all_pos):
+    item_batches = [items[i:i+batch_size] for i in range(0, len(items), batch_size)]
+    label_batches = [labels[i:i+batch_size] for i in range(0, len(labels), batch_size)]
+    pos_bathes = [all_pos[i:i+batch_size] for i in range(0, len(all_pos), batch_size)]
 
+    return item_batches, label_batches, pos_bathes
 
+# 位置编码
+def pos_encoding(items, vocab_size):
+    '''
+    :param items: data_item list
+    :param vocab_size
+    :return:
+    '''
+    pos_embedding = np.ones(vocab_size, 4)
+    for i, item in zip(range(0, len(items)), items):
+        pos_embedding[i][0] = i
+        pos_embedding[i][1] = i - item.sub[0]
+        pos_embedding[i][2] = i - item.obj[0]
+        pos_embedding[i][3] = item.sub[0] - item.obj[0]
+    pos_embedding /= len(items)
+    return pos_embedding
 
 if __name__ == '__main__':
       vocab =  Vocab('../../datas/pre_embeddings/glove/glove.6B.200d.txt', 200, True)
       labels, labels_index = read_labels('../../datas/SemEval2010-Task8/train/train_result.txt')
-      all_datas = read_datas('../../datas/SemEval2010-Task8/train/train.txt',vocab.word2id,labels,labels_index)
-      print(all_datas)
+      all_items, labels = read_datas('../../datas/SemEval2010-Task8/train/train.txt',vocab.word2id,labels,labels_index)
+      item_batches, label_batches = load_data(50, all_items, labels)
+      print(item_batches[:2][1]) # 这样的方式可以将 每一项都取出来
